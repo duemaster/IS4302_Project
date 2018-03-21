c379841f40eabc71072ba081d8b94b41306ec6f1
Paco Pan <stven0758@hotmail.com>
2018-03-21 23:45:49 +0800

    Airline App Frontend

--- Airline/App/src/app/app.module.ts
+++ Airline/App/src/app/app.module.ts
@@ -4,18 +4,19 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from "../pages/login/login";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    LoginPage,
     HomePage,
     TabsPage
   ],
@@ -27,7 +28,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    LoginPage,
     HomePage,
     TabsPage
   ],
