197ad9050df2d9e4172e5bccf2efefa9140e683f
Summer <chensiwei0729@gmail.com>
2018-03-25 14:07:41 +0800

    air app login page

--- Airline/App/src/pages/login/login.ts
+++ Airline/App/src/pages/login/login.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+
+
+  email: string;
+  password: string;
+
+  errorMessage: string;
+  @ViewChild('input') myInput;
+
 
   constructor(public navCtrl: NavController) {
 
