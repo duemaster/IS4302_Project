1414e47aad3e91ef9de113e3d9ec1053950300ac
Summer <chensiwei0729@gmail.com>
2018-03-26 17:23:53 +0800

    cargo app

--- GHA/App/src/pages/home/home.ts
+++ GHA/App/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {AboutPage} from "../about/about";
+import {LoginPage} from "../login/login";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  scan(){
+    this.navCtrl.push(AboutPage);
+  }
+
+  logout(){
+    this.navCtrl.setRoot(LoginPage);
   }
 
 }
