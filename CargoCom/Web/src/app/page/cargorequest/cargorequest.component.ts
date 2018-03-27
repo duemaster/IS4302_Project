88ab6ea3b4fea3f9ae55fb7d3d1c4994e777e7a1
siqjia <siqi940@gmail.com>
2018-03-27 12:58:55 +0800

    update
    cargoweb2

--- CargoCom/Web/src/app/page/cargorequest/cargorequest.component.ts
+++ CargoCom/Web/src/app/page/cargorequest/cargorequest.component.ts
@@ -17,26 +17,24 @@ export class CargoRequestComponent implements OnInit {
     cargoInfo = [{item:'furniture', weight:40, owner: 'SIA Cargo', status:'Ready'},
         {item:'toys', weight:70, owner: 'Cathay Cargo', status:'Ready'},];
 
-    serviceInfo = [{item:'Business Class Meal X30', agent:'SATS', status:'Approved'},
-        {item:'Economic Class Meal X150', agent:'SATS', status:'Approved'},
-        {item:'Blanket + Pillow X200', agent:'SATS', status:'Approved'},
-        {item:'NewsPaper X100', agent:'DNATA', status:'Approved'},];
-
-    displayedColumns = ['FlightNo', 'Date', 'Departure', 'Landing','Status', 'Option'];
+    displayedColumns = ['ItemType', 'Owner', 'Departure', 'Destination','DeliveryDate', 'Option'];
     dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-    flight = {
-        date: '',
-        flightNo: '',
+    cargorequest = {
+        itemType: '',
+        owner: '',
         departure: '',
-        landing: '',
-        status: '',
-        crew:'',
+        destination: '',
+        deliveryDate: '',
+        flight:''
     };
 
     isCreate = false;
 
-    crew = [{id: '001', name: 'Lily'}, {id: '002', name: 'Susan'}, {id: '003', name: 'Helen'}, {id: '004', name: 'Mary'}];
+    availFlight = [{flightNo: 'SQ851', date: '01-03-2018 08:20', departure: 'SIN', landing: 'CAN'},
+        {flightNo: 'SQ852', date: '01-03-2018 13:10', departure: 'CAN', landing: 'SIN'},
+        {flightNo: 'SQ800', date: '02-03-2018 02:20', departure: 'SIN', landing: 'PEK'},
+        {flightNo: 'SQ801', date: '02-03-2018 10:30', departure: 'PEK', landing: 'SIN'}];
 
     applyFilter(filterValue: string) {
         filterValue = filterValue.trim(); // Remove whitespace
@@ -52,36 +50,36 @@ export class CargoRequestComponent implements OnInit {
 
     update(element){
         this.isCreate = false;
-        this.flight = element;
+        this.cargorequest = element;
     }
 
     create() {
         this.isCreate = true;
-        this.flight = {
-            date: '',
-            flightNo: '',
+        this.cargorequest = {
+            itemType: '',
+            owner: '',
             departure: '',
-            landing: '',
-            status: '',
-            crew:'',
+            destination: '',
+            deliveryDate: '',
+            flight:''
         };
     }
 
 
 }
-export interface Flight {
-    date: string;
-    flightNo: string;
+export interface CargoRequest {
+    itemType: string;
+    owner: string;
     departure: string;
-    landing: string;
-    crew: string;
-    status: string
+    destination: string;
+    deliveryDate: string;
+    flight: string
 }
 
-const ELEMENT_DATA: Flight[] = [
-    {flightNo: 'SQ851', date: '01-03-2018 08:20', departure: 'SIN', landing: 'CAN', status: 'Scheduled', crew: '001'},
-    {flightNo: 'SQ852', date: '01-03-2018 13:10', departure: 'CAN', landing: 'SIN', status: 'Scheduled', crew: '002'},
-    {flightNo: 'SQ800', date: '02-03-2018 02:20', departure: 'SIN', landing: 'PEK', status: 'Scheduled', crew: '003'},
-    {flightNo: 'SQ801', date: '02-03-2018 10:30', departure: 'PEK', landing: 'SIN', status: 'Scheduled', crew: '004'},
+const ELEMENT_DATA: CargoRequest[] = [
+    {itemType: 'furniture', owner: '', departure: 'SIN', destination: 'CAN', deliveryDate: '01-03-2018 08:20', flight: ''},
+    {itemType: 'toy', owner: '', departure: 'CAN', destination: 'SIN', deliveryDate: '01-03-2018 13:10', flight: ''},
+    {itemType: 'electronics', owner: '', departure: 'SIN', destination: 'PEK', deliveryDate: '02-03-2018 02:20', flight: ''},
+    {itemType: 'kitchenware', owner: '', departure: 'PEK', destination: 'SIN', deliveryDate: '02-03-2018 10:30', flight: ''},
 
 ];
