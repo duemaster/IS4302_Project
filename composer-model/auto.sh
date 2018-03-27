25dad861438fa38310dd4e51ceb20610dae5b576
duemaster <zhangbsn@gmail.com>
2018-03-27 11:40:47 +0800

    Update Model

--- composer-model/auto.sh
+++ composer-model/auto.sh
@@ -4,19 +4,19 @@ docker exec -it ca.org1.example.com fabric-ca-client enroll -M registrar -u http
 
 # Register user
 PASSWORD="$(docker exec -it ca.org1.example.com fabric-ca-client register -M registrar -u http://localhost:7054 --id.name admin1 --id.affiliation org1 --id.attrs '"hf.Registrar.Roles=client"' --id.type user)";
-# Password is vltlThfGkrKy
+# PASSWORD="Password: vltlThfGkrKy";
+
+# echo $PASSWORD;
+PASSWORD=${PASSWORD##*"Password: "};
 
 echo $PASSWORD;
 
-$PASSWORD = ${PASSWORD#=*}
+# VQekeUScvLWi
 
-echo $PASSWORD
+docker exec cli composer network deploy --archiveFile /mnt/air-chain.bna -A admin1 -c PeerAdmin@hlfv1 -S $PASSWORD;
 
-# docker exec cli composer network deploy --archiveFile /mnt/air-chain.bna -A admin1 -c PeerAdmin@hlfv1 -S $password;
+docker exec cli composer card import -f admin1@air-chain.card;
 
-# docker exec cli composer card import -f admin1@air-chain.card;
-
-# docker exec -d cli composer-rest-server -c admin1@air-chain -p 3000;
-
+docker exec cli composer-rest-server -c admin1@air-chain -p 3000;
 
 # Create Airline Company with id="Airline1"
