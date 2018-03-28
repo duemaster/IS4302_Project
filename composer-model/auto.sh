faa991ce696d33ac2dd1839e013653fda9fc30ef
duemaster <zhangbsn@gmail.com>
2018-03-28 16:39:51 +0800

    Update

--- composer-model/auto.sh
+++ composer-model/auto.sh
@@ -8,14 +8,14 @@
 docker exec -it ca.org1.example.com fabric-ca-client enroll -M registrar -u http://admin:adminpw@localhost:7054;
 
 # Register user
-PASSWORD=$(docker exec -it ca.org1.example.com fabric-ca-client register -M registrar -u http://localhost:7054 --id.name admin1 --id.affiliation org1 --id.attrs '"hf.Registrar.Roles=client"' --id.type user;)
+docker exec -it ca.org1.example.com fabric-ca-client register -M registrar -u http://localhost:7054 --id.name admin1 --id.affiliation org1 --id.attrs '"hf.Registrar.Roles=client"' --id.type user;
 
 echo ${PASSWORD##*"Password: "} > myPassword.txt; 
 PASSWORD=$(cat myPassword.txt);
 PASSWORD=${PASSWORD//$'\n'/};
 echo $PASSWORD;
 
-docker exec cli composer network deploy --archiveFile /mnt/air-chain.bna -A admin1 -c PeerAdmin@hlfv1 -S $PASSWORD;
+docker exec cli composer network deploy --archiveFile /mnt/air-chain.bna -A admin1 -c PeerAdmin@hlfv1 -S XutccqwGRkRE;
 
 docker exec cli composer card import -f admin1@air-chain.card;
 
