4a2eee5554803ef60bfd22800d1a80b9b249f3e3
Paco Pan <stven0758@hotmail.com>
2018-03-28 17:46:33 +0800

    update

--- Airline/Server/app/util/VagrantUtil.java
+++ Airline/Server/app/util/VagrantUtil.java
@@ -2,19 +2,22 @@ package util;
 
 import models.User;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class VagrantUtil {
-    public static final String boxId = "fd8245a";
+    public static final String boxId = "9ba44a9";
 
     public static void importUserCard(User user) {
         // Create User Card and import in Fabric Composer
         try {
-            Runtime.getRuntime().exec(
+            Process p = Runtime.getRuntime().exec(
                     "vagrant ssh " + boxId + " " +
-                            "-- \"sudo docker exec cli composer identity issue -c admin1@air-chain -u " + user.getId() + " -a org.airline.airChain.AirlineEmployee#" + user.getId() + ";\" " +
-                            "\"sudo docker exec cli composer card import -f " + user.getUserCardName() + ".card;\""
+                            "-- sudo docker exec cli composer identity issue -c admin1@air-chain -u " + user.getId() + " -a org.airline.airChain.AirlineEmployee#" + user.getId() + "; " +
+                            "sudo docker exec cli composer card import -f " + user.getUserCardName() + ".card;"
             );
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -24,7 +27,7 @@ public class VagrantUtil {
         try {
             Runtime.getRuntime().exec(
                     "vagrant ssh " + boxId + " " +
-                            "-- \"sudo docker exec cli composer card delete --name " + user.getUserCardName() + ";\" "
+                            "-- sudo docker exec cli composer card delete --name " + user.getUserCardName() + "; "
             );
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,7 +38,7 @@ public class VagrantUtil {
         try {
             Process p = Runtime.getRuntime().exec(
                     "vagrant ssh " + boxId + " " +
-                            "-- \"sudo docker exec -d cli composer-rest-server -c " + user.getUserCardName() + " -p " + user.getPortNumber() + "\";"
+                            "-- sudo docker exec -d cli composer-rest-server -c " + user.getUserCardName() + " -p " + user.getPortNumber() + ";"
             );
         } catch (IOException e) {
             System.out.println("exception happened - here's what I know: ");
