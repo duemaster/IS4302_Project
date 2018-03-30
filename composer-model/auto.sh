139c0250c7d41cdf4734483c5bfbafe3e598aa46
duemaster <zhangbsn@gmail.com>
2018-03-31 01:01:00 +0800

    Update Deployment Script

--- composer-model/auto.sh
+++ composer-model/auto.sh
@@ -8,15 +8,22 @@
 docker exec -it ca.org1.example.com fabric-ca-client enroll -M registrar -u http://admin:adminpw@localhost:7054;
 
 # Register user
-docker exec -it ca.org1.example.com fabric-ca-client register -M registrar -u http://localhost:7054 --id.name admin1 --id.affiliation org1 --id.attrs '"hf.Registrar.Roles=client"' --id.type user;
+PASSWORD=$(docker exec -it ca.org1.example.com fabric-ca-client register -M registrar -u http://localhost:7054 --id.name admin1 --id.affiliation org1 --id.attrs '"hf.Registrar.Roles=client"' --id.type user);
 
-echo ${PASSWORD##*"Password: "} > myPassword.txt; 
+# Extract Password
+PASSWORD=${PASSWORD##*"Password: "};
+
+# Trim password (remove whitespace characters)
+PASSWORD="$(echo -e "${PASSWORD}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')";
+
+# Write password into .txt
+echo $PASSWORD > myPassword.txt
+
 PASSWORD=$(cat myPassword.txt);
-PASSWORD=${PASSWORD//$'\n'/};
 echo $PASSWORD;
 
-docker exec cli composer network deploy --archiveFile /mnt/air-chain.bna -A admin1 -c PeerAdmin@hlfv1 -S XutccqwGRkRE;
+docker exec cli composer network deploy --archiveFile /mnt/air-chain.bna -A admin1 -c PeerAdmin@hlfv1 -S $PASSWORD;
 
 docker exec cli composer card import -f admin1@air-chain.card;
 
-docker exec cli composer-rest-server -c admin1@air-chain -p 4000;
+docker exec cli composer-rest-server -c admin1@air-chain -p 3000;
