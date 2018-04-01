396eccff5a17f4cc41f39426c563d8ed3fdd5c58
duemaster <zhangbsn@gmail.com>
2018-04-01 22:54:38 +0800

    Add Automation for Blockchain Setup & Update Network

--- Airline/Server/app/Infrastructure/Initialization.java
+++ Airline/Server/app/Infrastructure/Initialization.java
@@ -1,13 +1,23 @@
 package Infrastructure;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.typesafe.config.Config;
 import controllers.ServeFileController;
+import models.User;
+import org.mindrot.jbcrypt.BCrypt;
 import play.Environment;
+import play.libs.Json;
 import util.VagrantUtil;
 
 import java.io.*;
+import java.util.List;
 
 import static controllers.ServeFileController.fs;
 
@@ -20,5 +30,87 @@ public class Initialization {
     public void Initialization(Environment environment, Config config) {
         System.out.println("Fetching Vagrant Box Id");
         VagrantUtil.boxId = VagrantUtil.fetchVagrantBoxId();
+
+        setUpDefaultAccounts();
+    }
+
+    private void setUpDefaultAccounts() {
+        //Check if  there exists any user
+        List<User> existingUserList = User.find.query()
+                .where()
+                .eq("role", User.ROLE_OFFICER)
+                .findList();
+
+        if (existingUserList.size() >= 1) {
+            System.out.println("User found. Not creating default user...");
+            return;
+        }
+
+        User defaultUser = new User();
+        defaultUser.setName("admin");
+        defaultUser.setPassword(BCrypt.hashpw("password", BCrypt.gensalt()));
+        defaultUser.setRole(User.ROLE_OFFICER);
+        defaultUser.save();
+
+        //Create User in Composer Network
+        ObjectNode userNode = Json.newObject();
+        userNode.put("$class", "org.airline.airChain.AirlineEmployee");
+        userNode.put("id", String.valueOf(defaultUser.getId()));
+        userNode.put("name", defaultUser.getName());
+        userNode.put("role", defaultUser.getRole());
+        userNode.put("company", User.COMPANY_ID);
+
+        try {
+            //Add User into composer
+            HttpResponse<JsonNode> memberReply =
+                    Unirest.post("http://localhost:3000/api/org.airline.airChain.AirlineEmployee")
+                            .header("accept", "application/json")
+                            .header("Content-Type", "application/json")
+                            .body(new JsonNode(userNode.toString()))
+                            .asJson();
+
+            //System.out.println(memberReply.getBody().toString());
+
+            HttpResponse<JsonNode> airLineReply =
+                    Unirest.get("http://localhost:3000/api/org.airline.airChain.AirlineCompany/" + User.COMPANY_ID)
+                            .header("accept", "application/json")
+                            .header("Content-Type", "application/json")
+                            .asJson();
+
+            //System.out.println(airLineReply.getBody().toString());
+
+            JsonNode airlineJsonNode = airLineReply.getBody();
+            ObjectNode airline = (ObjectNode) Json.parse(airlineJsonNode.toString());
+            //System.out.println(airline.toString());
+
+            ArrayNode employeeNode = (ArrayNode) airline.get("employees");
+
+            //If Employee Array is empty on blockchain
+            if (employeeNode == null) {
+                employeeNode = Json.newArray();
+            }
+
+            employeeNode.add(String.valueOf(defaultUser.getId()));
+            airline.set("employees", employeeNode);
+            //System.out.println(airline.toString());
+
+            //Save Airline Company
+            HttpResponse<JsonNode> airlineCompanyReply =
+                    Unirest.put("http://localhost:3000/api/org.airline.airChain.AirlineCompany/" + User.COMPANY_ID)
+                            .header("accept", "application/json")
+                            .header("Content-Type", "application/json")
+                            .body(new JsonNode(airline.toString()))
+                            .asJson();
+
+            //System.out.println(airlineCompanyReply.getBody().toString());
+
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        // Create User Card and import in Fabric Composer
+        VagrantUtil.importUserCard(defaultUser);
+        System.out.println("User: " + defaultUser.getName() + " created.");
+        return;
     }
 }
--- composer-model/auto.sh
+++ composer-model/auto.sh
@@ -30,4 +30,14 @@ docker exec cli composer network deploy --archiveFile /mnt/air-chain.bna -A admi
 
 docker exec cli composer card import -f admin1@air-chain.card;
 
+echo "Creating default Airline Company: Airline1"
+docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.AirlineCompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.AirlineCompany", "resources": [{"$class": "org.airline.airChain.AirlineCompany","id": "Airline1","name": "MyAirline1"}]}';
+
+echo "Creating default Cargo Company: Cargo1"
+docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.CargoCompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.CargoCompany", "resources": [{"$class": "org.airline.airChain.CargoCompany","id": "Cargo1","name": "MyCargo1"}]}';
+
+echo "Creating default GHA Company: GHA1"
+docker exec cli composer transaction submit --card admin1@air-chain -d '{"$class": "org.hyperledger.composer.system.AddAsset","registryType": "Asset","registryId": "org.airline.airChain.GHACompany", "targetRegistry" : "resource:org.hyperledger.composer.system.AssetRegistry#org.airline.airChain.GHACompany", "resources": [{"$class": "org.airline.airChain.GHACompany","id": "GHA1","name": "MyGHA1"}]}';
+
+echo "Start REST Server at Port: 3000"
 docker exec cli composer-rest-server -c admin1@air-chain -p 3000;
