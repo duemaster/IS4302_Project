c8edac98f6ee08e12a881aae1819980d4544a8d6
duemaster <zhangbsn@gmail.com>
2018-04-03 00:25:40 +0800

    Port Angular into Play

--- Airline/Server/app/controllers/ServeFileController.java
+++ Airline/Server/app/controllers/ServeFileController.java
@@ -2,11 +2,27 @@ package controllers;
 
 import play.mvc.Controller;
 import play.data.FormFactory;
+import play.mvc.Result;
+
 import javax.inject.*;
+import java.io.File;
 
 public class ServeFileController extends Controller {
 
-    @Inject FormFactory formFactory;
+    @Inject
+    FormFactory formFactory;
     public static String fs = System.getProperty("file.separator");
     public static String path;
+
+    public Result adminPortal(String file) {
+
+        String angularDirectory = "airline_web_dist";
+        File f = new File(angularDirectory + fs + file);
+
+        if (!f.exists()) {
+            f = new File("airline_web_dist" + fs + "index.html");
+        }
+
+        return ok(f);
+    }
 }
