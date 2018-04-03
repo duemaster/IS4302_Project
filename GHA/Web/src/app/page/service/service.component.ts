ad6e74414c04167d05c5061576e6b195762ae2bc
Paco Pan <stven0758@hotmail.com>
2018-04-03 11:43:36 +0800

    Update_GHA

--- GHA/Web/src/app/page/service/service.component.ts
+++ GHA/Web/src/app/page/service/service.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatTableDataSource, MatPaginator} from '@angular/material';
+import {BlockChainService} from "../../service/blockchain/block-chain.service";
+import {AuthService} from "../../service/auth.service";
+import {HttpClient} from "@angular/common/http";
+import {SettingService} from "../../service/setting/setting.service";
 
 @Component({
   selector: 'app-service',
@@ -8,13 +12,16 @@ import {MatTableDataSource, MatPaginator} from '@angular/material';
 })
 export class ServiceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              private service: SettingService,
+              public authService: AuthService,
+              public blockChainService: BlockChainService) { }
 
   ngOnInit() {
   }
     displayedColumns = ['id', 'Type', 'Description', 'Status', 'Flight', 'Option'];
-    dataSource = new MatTableDataSource(ELEMENT_DATA);
-    service: any = {
+    dataSource = new MatTableDataSource([]);
+    serviceInfo: any = {
         type: '',
         id: '',
         description: '',
@@ -23,18 +30,9 @@ export class ServiceComponent implements OnInit {
         flight: {},
         flightList: ''
     };
-    isCreate = false;
 
-    flight = {
-        flightNo: '', date: '', departure: '', landing: '', status: '', crew: ''
-    };
 
-    staff = [{id: '001', name: 'Lily'}, {id: '002', name: 'Susan'}, {id: '003', name: 'Helen'}, {id: '004', name: 'Mary'}];
-    flightList = [{id: 'SQ851'}, {id: 'SQ852'}, {id: 'SQ800'}, {id: 'SQ801'}];
-    // flight = [{flightNo: 'SQ851', date: '01-03-2018 08:20', departure: 'SIN', landing: 'CAN', status: 'Scheduled', crew: '001'},
-    //     {flightNo: 'SQ852', date: '01-03-2018 13:10', departure: 'CAN', landing: 'SIN', status: 'Scheduled', crew: '002'},
-    //     {flightNo: 'SQ800', date: '02-03-2018 02:20', departure: 'SIN', landing: 'PEK', status: 'Scheduled', crew: '003'},
-    //     {flightNo: 'SQ801', date: '02-03-2018 10:30', departure: 'PEK', landing: 'SIN', status: 'Scheduled', crew: '004'},]
+    flight:any;
 
     applyFilter(filterValue: string) {
         filterValue = filterValue.trim(); // Remove whitespace
@@ -46,44 +44,46 @@ export class ServiceComponent implements OnInit {
 
     ngAfterViewInit() {
         this.dataSource.paginator = this.paginator;
+        this.fetchServiceList();
     }
 
-    create() {
-        this.isCreate = true;
-        this.service = {
-            type: '',
-            id: '',
-            description: '',
-            status: '',
-            staff: '',
-            flight: {},
-        };
-    }
+
 
     update(element) {
-        this.isCreate = false;
-        this.service = element;
+        this.serviceInfo = element;
     }
 
-    viewFlight(element){
-        this.flight = element;
+    async viewFlight(flight){
+        this.flight = await this.http.get(
+            `${this.service.ENDPOINT}/blockchain/user/${this.authService.admin.id}/api/org.airline.airChain.Flight/${flight}`,
+            {withCredentials: true}
+        ).toPromise();
+        //Remove Cabin Crew NameSpace
+        if(flight.cabinCrews) {
+            flight.cabinCrews = flight.cabinCrews.map((cabinCrew) => {
+                return cabinCrew.replace(this.blockChainService.AIRLINE_EMPLOYEE, "");
+            });
+        }
+
     }
+    async fetchServiceList() {
+        let serviceList: any = await this.http.get(
+            `${this.service.ENDPOINT}/blockchain/user/${this.authService.admin.id}/api/org.airline.airChain.Service`,
+            {withCredentials: true}
+        ).toPromise();
+
+
+        console.log(serviceList);
+
+        this.loadDataInTable(serviceList);
+    }
+
+    loadDataInTable(serviceList) {
+        this.dataSource = new MatTableDataSource<any>(serviceList);
+        this.dataSource.paginator = this.paginator;
+    }
+
+
 }
 
-export interface Service {
-    type: string;
-    id: string;
-    description: string;
-    status: string
-    staff: string
-    flight: {}
-}
 
-const ELEMENT_DATA: Service[] = [
-    {id: '9VSTC', type: 'Food', description: 'Provide Catering', status: 'PENDING', staff: '001', flight: {flightNo: 'SQ851', date: '01-03-2018 08:20', departure: 'SIN', landing: 'CAN', status: 'Scheduled', crew: '001'}},
-    {id: '9VPTV', type: 'Utilities', description: 'Clean Aircraft', status: 'APPROVED', staff: '002', flight: {flightNo: 'SQ851', date: '01-03-2018 08:20', departure: 'SIN', landing: 'CAN', status: 'Scheduled', crew: '001'}},
-    {id: '9VSSG', type: 'Utilities', description: 'Clean Aircraft', status: 'REJECTED', staff: '002', flight: {flightNo: 'SQ852', date: '01-03-2018 13:10', departure: 'CAN', landing: 'SIN', status: 'Scheduled', crew: '002'}},
-    {id: '9VOTS', type: 'Food', description: 'Provide Catering', status: 'DONE', staff: '003', flight: {flightNo: 'SQ852', date: '01-03-2018 13:10', departure: 'CAN', landing: 'SIN', status: 'Scheduled', crew: '002'}},
-    {id: '9VOTM', type: 'Food', description: 'Provide Catering', status: 'NOT DONE', staff: '004', flight: {flightNo: 'SQ800', date: '02-03-2018 02:20', departure: 'SIN', landing: 'PEK', status: 'Scheduled', crew: '003'}},
-
-];
