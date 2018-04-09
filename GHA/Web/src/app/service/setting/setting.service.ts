f0bc00766c7167a88a3c6d14c3e80220f9593240
Summer <chensiwei0729@gmail.com>
2018-04-09 13:58:55 +0800

    change

--- GHA/Web/src/app/service/setting/setting.service.ts
+++ GHA/Web/src/app/service/setting/setting.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SettingService {
 
-    public readonly ENDPOINT = 'http://localhost:9000';
+    public readonly ENDPOINT = 'http://localhost:9001';
     public loading = false;
   constructor() { }
 
