c87861f82be697a240974068973b92aa30a80630
Summer <chensiwei0729@gmail.com>
2018-04-16 17:19:47 +0800

    change

--- GHA/Server/app/controllers/ServeFileController.java
+++ GHA/Server/app/controllers/ServeFileController.java
@@ -1,12 +1,30 @@
 package controllers;
-
 import play.mvc.Controller;
 import play.data.FormFactory;
+import play.mvc.Result;
+
 import javax.inject.*;
+import java.io.File;
 
 public class ServeFileController extends Controller {
 
     @Inject FormFactory formFactory;
     public static String fs = System.getProperty("file.separator");
     public static String path;
+
+    public Result adminPortal(String file) {
+
+        String angularDirectory = "dist";
+        File f = new File(angularDirectory + fs + file);
+
+        if (!f.exists()) {
+            f = new File("dist" + fs + "index.html");
+        }
+
+        return ok(f);
+    }
+
+    public Result adminPortalIndex() {
+        return ok(new File("dist" + fs + "index.html"));
+    }
 }
